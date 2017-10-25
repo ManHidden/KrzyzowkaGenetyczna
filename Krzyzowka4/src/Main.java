dfde898259b486a2cb6f10042a9462f59bbd3647
Patryk <hiddenmanpl@gmail.com>
2017-10-25 22:21:01 +0200

    Zmiana 12 linii z gita na chuj w celu testu

---
+++ Krzyzowka4/src/Main.java
@@ -0,0 +1,14 @@
+package src;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args){
+
+        Rodzice rodzice = new Rodzice("AaBb", "AaBb");
+
+        System.out.println(rodzice);
+        System.out.println("Chuj");
+    }
+}

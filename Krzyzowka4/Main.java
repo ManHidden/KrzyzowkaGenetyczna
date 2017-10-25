9d5bf469a85064df427dfc05ac5cbdb593f675bd
ManHidden <32382445+ManHidden@users.noreply.github.com>
2017-10-25 17:13:04 +0200

    Add files via upload

---
+++ Krzyzowka4/Main.java
@@ -0,0 +1,14 @@
+package Krzyzowka4;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args){
+
+        Rodzice rodzice = new Rodzice("AaBb", "AaBb");
+
+        System.out.println(rodzice);
+        System.out.println("Git");
+    }
+}

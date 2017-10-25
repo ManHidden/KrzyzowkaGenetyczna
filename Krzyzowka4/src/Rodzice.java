5ff296bbcc45c1a4dc8691a0fb9574bdc0cd3022
Patryk <hiddenmanpl@gmail.com>
2017-10-25 23:08:57 +0200

    Proba ogarniecia alfabetyzacji tablic

--- Krzyzowka4/src/Rodzice.java
+++ Krzyzowka4/src/Rodzice.java
@@ -1,6 +1,15 @@
 package src;
 
+import java.sql.Array;
 import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+    1. Zautomatyzować linijki 31-40 i 45-55
+    2. Ułożyć elemetny w tablicy według alfabetu AABB
+
+
+ */
 
 public class Rodzice {
 
@@ -8,7 +17,7 @@ public class Rodzice {
     private String Rodzic2;
 
     //Konstruktor rodziców
-    public Rodzice(String Rodzic1, String Rodzic2){
+    public Rodzice(String Rodzic1, String Rodzic2) {
         this.Rodzic1 = Rodzic1;
         this.Rodzic2 = Rodzic2;
 
@@ -20,11 +29,10 @@ public class Rodzice {
         Arrays.sort(Rodzic2WTablicy);
 
         //Tworze AB, Ab, aB, ab
-        //                          ***Trzeba to zautomatyzować***
         /*
-            0  1  2  3
-            A, a, B, b
-            A, a, B, b
+                            0  1  2  3
+            Rodzic1WTablicy A, a, B, b
+            Rodzic2WTablicy A, a, B, b
         */
         String pierwszyAllel = Rodzic1WTablicy[0] + Rodzic2WTablicy[1]; //AB
         String drugiAllel = Rodzic1WTablicy[0] + Rodzic2WTablicy[3];    //Ab
@@ -33,41 +41,49 @@ public class Rodzice {
 
         System.out.println("1 " + pierwszyAllel + " 2 " + drugiAllel + " 3 " + trzeciAllel + " 4 " + czwartyAllel);
 
-        String[][] krzyzowkaGenetyczna = new String[Rodzic1.length()+1][Rodzic2.length()+1];
-        krzyzowkaGenetyczna [0][0] = null;
+        String[][] krzyzowkaGenetyczna = new String[Rodzic1.length() + 1][Rodzic2.length() + 1];
+        krzyzowkaGenetyczna[0][0] = null;
 
-        krzyzowkaGenetyczna [0][1] = pierwszyAllel;
-        krzyzowkaGenetyczna [0][2] = drugiAllel;
-        krzyzowkaGenetyczna [0][3] = trzeciAllel;
-        krzyzowkaGenetyczna [0][4] = czwartyAllel;
+        krzyzowkaGenetyczna[0][1] = pierwszyAllel;
+        krzyzowkaGenetyczna[0][2] = drugiAllel;
+        krzyzowkaGenetyczna[0][3] = trzeciAllel;
+        krzyzowkaGenetyczna[0][4] = czwartyAllel;
 
-        krzyzowkaGenetyczna [1][0] = pierwszyAllel;
-        krzyzowkaGenetyczna [2][0] = drugiAllel;
-        krzyzowkaGenetyczna [3][0] = trzeciAllel;
-        krzyzowkaGenetyczna [4][0] = czwartyAllel;
+        krzyzowkaGenetyczna[1][0] = pierwszyAllel;
+        krzyzowkaGenetyczna[2][0] = drugiAllel;
+        krzyzowkaGenetyczna[3][0] = trzeciAllel;
+        krzyzowkaGenetyczna[4][0] = czwartyAllel;
 
-        for (int i = 1; i <= Rodzic1.length(); i++){
+        for (int i = 1; i <= Rodzic1.length(); i++) {
             //krzyzowkaGenetyczna[0][i] = null;
             //krzyzowkaGenetyczna[i][0] = null;
 
-                //[1][1], [2][1], [3][1], [4][1]
-                krzyzowkaGenetyczna[i][1] = krzyzowkaGenetyczna[i][0] + krzyzowkaGenetyczna[0][1];
+            //[1][1], [2][1], [3][1], [4][1]
+            krzyzowkaGenetyczna[i][1] = krzyzowkaGenetyczna[i][0] + krzyzowkaGenetyczna[0][1];
 
-                //[1][2], [2][2], [3][2], [4][2]
-                krzyzowkaGenetyczna[i][2] = krzyzowkaGenetyczna[i][0] + krzyzowkaGenetyczna[0][2];
+            //[1][2], [2][2], [3][2], [4][2]
+            krzyzowkaGenetyczna[i][2] = krzyzowkaGenetyczna[i][0] + krzyzowkaGenetyczna[0][2];
 
-                //[1][3], [2][3], [3][3], [4][3]
-                krzyzowkaGenetyczna[i][3] = krzyzowkaGenetyczna[i][0] + krzyzowkaGenetyczna[0][3];
+            //[1][3], [2][3], [3][3], [4][3]
+            krzyzowkaGenetyczna[i][3] = krzyzowkaGenetyczna[i][0] + krzyzowkaGenetyczna[0][3];
 
-                //[1][4], [2][4], [3][4], [4][4]
-                krzyzowkaGenetyczna[i][4] = krzyzowkaGenetyczna[i][0] + krzyzowkaGenetyczna[0][4];
+            //[1][4], [2][4], [3][4], [4][4]
+            krzyzowkaGenetyczna[i][4] = krzyzowkaGenetyczna[i][0] + krzyzowkaGenetyczna[0][4];
 
-            //Arrays.sort(krzyzowkaGenetyczna);
-                for(int j = 1; j <= 4; j++){
-                    System.out.println(i + "." + j +". " + krzyzowkaGenetyczna[i][j]);
-                }
-
+            //Wypisuje wszystkie elementy tablicy
+            for (int j = 1; j <= 4; j++) {
+                System.out.println(i + "." + j + ". " + krzyzowkaGenetyczna[i][j]);
+            }
         }
+                            //**********************************//
+        String[] krzyzowkaAlfabetycznie = new String[2];
+        krzyzowkaAlfabetycznie [0] = krzyzowkaGenetyczna[1][1];
+        krzyzowkaAlfabetycznie [1] = krzyzowkaGenetyczna[1][2];
 
+        System.out.println("Przed sortowaniem: " + krzyzowkaAlfabetycznie[0] + " i " + krzyzowkaAlfabetycznie[1]);
+
+        Arrays.sort(krzyzowkaAlfabetycznie);
+        System.out.println("Po sortowaniu: " + krzyzowkaAlfabetycznie[0] + " i " + krzyzowkaAlfabetycznie[1]);
+                            //**********************************//
     }
 }
